08bf8d41255e391c3782c2fa155d1a28cdd110d2
pkbtc <parasarkashyap06@gmail.com>
2026-04-30 15:40:03 +0530

    .

--- frontend/src/constants/UpcomingSpeakers.js
+++ frontend/src/constants/UpcomingSpeakers.js
@@ -7,14 +7,7 @@ const upcomingSpeakers = [
     bio: "Rituraj Phukan is a globally recognized climate activist, author, and TEDx speaker from Assam. He is the founder of the Indigenous People’s Climate Justice Forum and has spoken at multiple UN Climate Conferences including COP26–COP29. A Climate Reality Leader trained by Al Gore, he has led large-scale environmental initiatives, including planting over 2.7 million trees.",
     revealDate: "2026-04-07T18:00:00",
   },
-  {
-    id: 1,
-    name: "Captain Shruti Chauhan",
-    title: "Commercial Pilot & Aviation Trailblazer",
-    image: "/images/speakers/cap.jpeg",
-    bio: "Captain Shruti Chauhan is a commercial pilot known for breaking barriers in the aviation industry. She has inspired many through her journey in a traditionally male-dominated field, advocating for women in aviation and STEM.",
-    revealDate: "2026-04-07T14:00:00",
-  },
+
   {
     id: 3,
     name: "Dr. Nilima Roy Chowdhury",
